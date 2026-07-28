1662cbc49a8e4bd88f4aad36d10977715f0227a6
Paul Fauchon <paul@logit.global>
2026-07-28 12:29:57 +0800

    fix linux build

--- shell.nix
+++ shell.nix
@@ -16,12 +16,14 @@ let
 in
 pkgs.mkShell {
   buildInputs = with pkgs; builtins.concatLists [
-    [ flutter git http-server serverpod_cli fastlane gst_all_1.gstreamer
+    [ flutter git http-server serverpod_cli fastlane
       gst_all_1.gstreamer
            gst_all_1.gst-plugins-base
            gst_all_1.gst-plugins-good
            gst_all_1.gst-plugins-bad
            gtk3
+           pkg-config
+           sysprof
      ]
   ];
 
@@ -38,15 +40,15 @@ pkgs.mkShell {
   dart run tools/setup_git_hooks.dart
 
   echo "Adding flutter submodule to path"
-  export PATH="./submodules/flutter/bin:$PATH"
+  export PATH="${toString ./.}/submodules/flutter/bin:$PATH"
 
 
   flutter config --jdk-dir ${pkgs.jdk21}/lib/openjdk
 
   echo "Exporting android auto emulator libraries"
-   export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.libcxx pkgs.libcxxrt pkgs.gtk3 pkgs.gst_all_1.gstreamer pkgs.gst_all_1.gstreamermm pkgs.libunwind ]}:$LD_LIBRARY_PATH"
-    export PKG_CONFIG_PATH="${pkgs.lib.makeSearchPathOutput "dev" "lib/pkgconfig" [ pkgs.gst_all_1.gstreamer pkgs.gst_all_1.gst-plugins-base pkgs.libunwind pkgs.gtk3 ]}:$PKG_CONFIG_PATH"
-
+  export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.libcxx pkgs.libcxxrt pkgs.gtk3 pkgs.gst_all_1.gstreamer pkgs.gst_all_1.gstreamermm pkgs.libunwind ]}:$LD_LIBRARY_PATH"
+  export PKG_CONFIG_PATH="${pkgs.lib.makeSearchPathOutput "dev" "lib/pkgconfig" [ pkgs.gst_all_1.gstreamer pkgs.gst_all_1.gst-plugins-base pkgs.libunwind pkgs.gtk3 pkgs.sysprof ]}:$PKG_CONFIG_PATH"
+  unset DESTDIR
   ''+
           pkgs.lib.concatStrings (map (x: ''echo "${x.name}: ${x.description}";'') aliases);
 
