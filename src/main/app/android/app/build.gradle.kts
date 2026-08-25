b9fe27617818d9ff2424ee81defdd1cfe5657176
office-pc nix root <paul@fauchon.dev>
2026-08-25 09:53:21 +0800

    add proper build number and version from tag and github build number

--- src/main/app/android/app/build.gradle.kts
+++ src/main/app/android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = System.getenv("TAG_NAME")?.toIntOrNull() ?: flutter.versionCode
+        versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
